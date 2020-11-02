1ff3e4fb4a5f8784ad40e6d938f57deed94ab97a
Dimitris Mpilas <38569487+jimis189@users.noreply.github.com>
2020-11-03 01:56:00 +0200

    elegxos ean to username yparxei sth bash

--- doregister.php
+++ doregister.php
@@ -8,10 +8,33 @@ $password2 = mysqli_real_escape_string($mysqli,$_POST['password2']);
 $password1 = md5($password1);
 
 
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['username_check']) && $_POST['username_check'] == 1 ) {
+
+	// validate username
+
+	
+
+	$sqlcheck = "SELECT username FROM users WHERE username = '$username' ";
+
+	$checkResult = $mysqli->query($sqlcheck);
+
+	// check if username already taken
+	if(mysqli_num_rows($checkResult) > 0) {
+		header("HTTP/1.1 500 Internal Server Error");
+	}
+}
+
+else if($username !="" && $password1 != '' && $password2 != ''){
+	
  $sql ="INSERT INTO `users`(`player_id`,`username`,`password`) VALUES ('','$username','$password1')";
  mysqli_query($mysqli,$sql);
 
- header('Location: game.php');
- print json_encode(array('success'=>'ok'));
+
+ 
+} else{
+	header("HTTP/1.1 500 Internal Server Error");
+}
+
+
 
 ?>
--- js/register.js
+++ js/register.js
@@ -1,3 +1,32 @@
+//check if username exists
+var usernameExists = true;
+$("#username").blur(function(e) {
+    e.preventDefault();
+    const username = $('#username').val();
+    if (username == '') {
+        return;
+    }
+
+    $.ajax({
+        url: 'doregister.php',
+        type: "post",
+        data: {
+            username: username,
+            username_check: 1
+        },
+        success: function(response) {
+            usernameExists = false;
+        },
+        error: function(x) {
+            usernameExists = true;
+            alert('Username exists try another');
+            console.log("fail");
+        }
+    });
+
+
+});
+
 // Register Confirm Password Validation
 
 $('#registerbtn').on('click', function() {
