38f17ed79fc6870d4aa0d84bf0ad9f5ccaeb0e68
Dimitris Mpilas <38569487+jimis189@users.noreply.github.com>
2020-12-09 21:23:20 +0200

    clear tables

--- home/dologout.php
+++ home/dologout.php
@@ -4,7 +4,11 @@ session_start();
 
 $username = $_SESSION['username'];
 $sql = "UPDATE `users` SET `player`=''  WHERE username = '$username'";
+$sql1 = "DELETE FROM `chat` ";
+$sql2 = "DELETE FROM  `status_turn` ";
 mysqli_query($mysqli,$sql);
+mysqli_query($mysqli,$sql1);
+mysqli_query($mysqli,$sql2);
 session_unset();
 session_destroy();
 
